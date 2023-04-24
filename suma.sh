06de1f1ee475b0031e1d68493f31b73f93b32b77
adrihierro <aadrihp@gmail.com>
2023-04-24 09:38:02 +0200

    Suma pero con control de errores

--- suma.sh
+++ suma.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#control d'errades
+#Si no ens passen 2 paràmetres donam un missatge de error i sortim del script 
+NUM_PARAM=${#}
+#echo "NUM_PARAM ${NUM_PARAM}"
+if [[ ${NUM_PARAM} -ne 2 ]]
+then
+    echo "ERROR: has d'introduir dos parametres"
+    exit 1
+fi
 
 A=${1}
 B=${2}
