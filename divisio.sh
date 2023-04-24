0b47d387349d7edf4bddb14c02e06b48c31c33d0
adrihierro <aadrihp@gmail.com>
2023-04-24 09:43:27 +0200

    Division con resultado decimal

--- divisio.sh
+++ divisio.sh
@@ -7,5 +7,5 @@ B=${2}
 #Scale serveix per especificar el numero de decimals que tendra la 
 #divisio i bc es la eina el cual fa posible que la divisio doni decimals
 SUM=$(echo "scale=2; $A / $B" | bc)
-echo "${A} + ${B}= ${SUM}"
+echo "${A} / ${B}= ${SUM}"
 echo "El resultat de la divisio es ${SUM}"
